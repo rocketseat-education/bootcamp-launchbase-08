07a5d8b15a5bc3a4a75b7a0e9239458b6f93ff1b
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-23 00:46:46 -0400

    13 Estrutura da Listagem de Anúncios  do Usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,7 @@ const { hash } = require('bcryptjs')
 
 const User = require('../models/User')
 const Product = require('../models/Product')
+const LoadProductsService = require('../services/LoadProductService')
 
 const { formatCep, formatCpfCnpj } = require('../../lib/utils')
 
@@ -113,5 +114,12 @@ module.exports = {
                 error: "Erro ao tentar deletar sua conta!"
             })
         }
+    },
+    async ads( req, res) {
+        const products = await LoadProductsService.load('products', {
+            where: { user_id: req.session.userId }
+        })
+
+        return res.render("user/ads", { products })
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -28,5 +28,7 @@ routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/', UserValidator.update, UserController.update)
 routes.delete('/', UserController.delete)
 
+routes.get('/ads', UserController.ads)
+
 
 module.exports = routes
