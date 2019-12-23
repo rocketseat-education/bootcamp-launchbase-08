08f286732cd4c25269f326e40be36605922b33e1
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-22 23:41:28 -0400

    12 Validação Back e Front end dos Produtos.

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -17,18 +17,6 @@ module.exports = {
     },
     async post(req, res) {
         try {
-
-            const keys = Object.keys(req.body)
-
-            for (key of keys) {
-                if (req.body[key] == "") {
-                    return res.send('Please, fill all fields!')
-                }
-            }
-
-            if (req.files.length == 0)
-                return res.send('Please, send at least one image')
-
             let { category_id, name, description, old_price,
                 price, quantity, status } = req.body
 
@@ -87,15 +75,6 @@ module.exports = {
     },
     async put(req, res) {
         try {
-
-            const keys = Object.keys(req.body)
-
-            for (key of keys) {
-                if (req.body[key] == "" && key != "removed_files") {
-                    return res.send('Please, fill all fields!')
-                }
-            }
-
 
             if (req.files.length != 0) {
                 const newFilesPromise = req.files.map(file =>
